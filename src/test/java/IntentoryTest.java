e5635ae687a8ad96f6886674d893abde87daed47
Milos Boskovic <milos.c.boskovic@gmailcom>
2023-07-06 20:42:24 +0200

    Add remove product

--- src/test/java/IntentoryTest.java
+++ src/test/java/IntentoryTest.java
@@ -95,6 +95,17 @@ public class IntentoryTest extends BaseTest
         Assert.assertEquals(checkoutStepOnePage.getError(),"Error: First Name is required");
 
     }
+    @Test
+    public void RemoveProductFromCart()
+    {
+        inventoryPage.clickLight();
+        inventoryPage.clickBackpack();
+        inventoryPage.clickCart();
+        inventoryPage.removeBackpack();
+
+        Assert.assertEquals(inventoryPage.getCartNumber(),"1");
+
+    }
 
     @AfterMethod
     public void after()
